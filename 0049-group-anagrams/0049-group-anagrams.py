636d0ee291da7c32748ea87f8b62f347263c90dd
Kenny Thai <thaikenny2003@gmail.com>
2023-10-04 21:00:44 -0700

    Time: 114 ms (18.13%), Space: 22.1 MB (23.91%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,17 +1,11 @@
-class Solution(object):
-    def groupAnagrams(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
-
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         res = defaultdict(list)
-
-        for str in strs:
-            count = [0] * 26
-            for c in str:
-                count[ord(c) - ord('a')] += 1
+        for s in strs:
+            char = [0] * 26
+            for c in s:
+                char[ord(c) - ord('a')] += 1
             
-            res[tuple(count)].append(str)
+            res[tuple(char)].append(s)
         
         return res.values()
