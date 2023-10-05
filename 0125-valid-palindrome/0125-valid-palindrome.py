e3dab56fd2e28c5d9766ad943a6924a5458c01b5
Kenny Thai <thaikenny2003@gmail.com>
2023-10-05 16:58:13 -0700

    Time: 69 ms (10.17%), Space: 17.1 MB (78.18%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,24 +1,17 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
         l, r = 0, len(s) - 1
         while l < r:
-            while l < r and not self.alphaNum(s[l]):
+            while l < r and not self.isAlphaNum(s[l]):
                 l += 1
-
-            while l < r and not self.alphaNum(s[r]):
+            while l < r and not self.isAlphaNum(s[r]):
                 r -= 1
-            
-            if s[l].lower() != s[r].lower():
+            if s[l].lower()!= s[r].lower():
                 return False
-            
             l, r = l + 1, r - 1
         return True
-
-    def alphaNum(self, c):
+    
+    def isAlphaNum(self, c) -> bool:
         return (ord('a') <= ord(c) <= ord('z') or
                 ord('A') <= ord(c) <= ord('Z') or
                 ord('0') <= ord(c) <= ord('9'))
