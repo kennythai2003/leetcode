fe70e8bb984624d8f68ed2c76f225e17c81fd0b9
Kenny Thai <thaikenny2003@gmail.com>
2023-10-04 20:48:36 -0700

    Time: 487 ms (30.99%), Space: 34.4 MB (6.98%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,16 +1,10 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
         hashmap = {}
-
         for num in nums:
-
             if num not in hashmap:
                 hashmap[num] = 1
             else:
                 return True
-        
+            
         return False
