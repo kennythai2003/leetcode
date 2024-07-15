04cb7848a2ee0cca9e7bda0992d8a0d59ca655a8
Kenny Thai <thaikenny2003@gmail.com>
2024-07-14 23:47:59 -0700

    Time: 96 ms (23.28%), Space: 21.9 MB (19.9%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,28 +1,20 @@
-class Solution(object):
-    def topKFrequent(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
-        
-        #  hashmap to count elements
-        # inverse array
-        # loop through array
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        numsmap = {}
+        freq = [[] for i in range(len(nums) + 1)]
+        result = []
 
-        hashmap = {}
-
-        for num in nums:
-            hashmap[num] = 1 + hashmap.get(num, 0)
+        for n in nums:
+            numsmap[n] = 1 + numsmap.get(n, 0)
         
-        arr = [[] for i in range(len(nums) + 1)]
 
-        for i, v in hashmap.items():
-            arr[v].append(i)
+        for i, v in numsmap.items():
+            freq[v].append(i)
         
-        res = []
-        for i in range(len(arr) - 1, -1, -1):
-            for j in arr[i]:
-                res.append(j)
-                if len(res) == k:
-                    return res
+        for i in range(len(freq) - 1, -1, -1):
+
+            for j in freq[i]:
+                result.append(j)
+                if len(result) == k:
+                    return result
+        
