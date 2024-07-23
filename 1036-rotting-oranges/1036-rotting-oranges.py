b91212b3b9c35a44861bb47a668b965e18e30f94
Kenny Thai <thaikenny2003@gmail.com>
2024-07-23 00:09:14 -0700

    Time: 43 ms (91.71%), Space: 16.7 MB (15.51%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,40 +1,35 @@
-class Solution(object):
-    def orangesRotting(self, grid):
-        """
-        :type grid: List[List[int]]
-        :rtype: int
-        """
-        ROWS, COLS = len(grid), len(grid[0])
-        queue = deque()
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        ROWS = len(grid)
+        COLS = len(grid[0])
+        
+        fresh = 0
+        mins = 0
+
         directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
 
-        fresh = 0
-        minutes = 0
-
-        for r in range(len(grid)):
-            for c in range(len(grid[0])):
-                if grid[r][c] == 1:
-                    fresh += 1
-                if grid[r][c] == 2:
-                    queue.append((r, c))
-        
-        while fresh > 0 and queue:
-            for i in range(len(queue)):
-                r, c = queue.popleft()
-
-                for dr, dc in directions:
-                    if (r + dr < 0 or c + dc < 0 or
-                        r + dr == ROWS or c + dc == COLS or
-                        grid[r + dr][c + dc] == 0 or grid[r + dr][c + dc] == 2):
-                        continue
-                    grid[r + dr][c + dc] = 2
-                    queue.append((r + dr, c + dc))
-                    fresh -= 1
-            minutes += 1
-
-        if fresh == 0:
-            return minutes
-        else:
+        if not grid:
             return -1
+
+        queue = deque()
+        for i in range(ROWS):
+            for j in range(COLS):
+                if grid[i][j] == 1:
+                    fresh += 1
+                elif grid[i][j] == 2:
+                    queue.append((i, j))
+
+        while queue and fresh > 0:
+            for i in range(len(queue)):
+                row, col = queue.popleft()
+                for dr, dc in directions:
+                    newrow, newcol = row + dr, col + dc
+                    if (newrow < 0 or newcol < 0 or newrow >= ROWS or newcol >= COLS or grid[newrow][newcol] == 2 or grid[newrow][newcol] == 0):
+                        continue
                     
-            
+                    grid[newrow][newcol] = 2
+                    queue.append((newrow, newcol))
+                    fresh -= 1
+            mins +=  1
+
+        return mins if not fresh else -1
