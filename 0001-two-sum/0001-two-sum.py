922c88bc7b65c016d2492a7066381c1cbfd28e47
Kenny Thai <thaikenny2003@gmail.com>
2024-07-29 16:57:04 -0700

    Time: 46 ms (57.6%), Space: 12.5 MB (67.61%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,17 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        prevdiffs = {}
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+        diffs = {}
 
-        for i, n in enumerate(nums):
-
-            if target - n in prevdiffs:
-                return [prevdiffs[target - n], i]
-            
-            prevdiffs[n] = i
+        for i, v in enumerate(nums):
+            diff = target - v
+            if diff in diffs:
+                return [diffs[diff], i]
+            diffs[v] = i
         
         return []
