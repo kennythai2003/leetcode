01dbb9fd4e6efa5ebc1a77313f5008b670c47a4d
Kenny Thai <thaikenny2003@gmail.com>
2024-07-29 17:57:13 -0700

    Time: 411 ms (76.37%), Space: 18.2 MB (27.95%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
@@ -2,37 +2,31 @@ class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         ROWS = len(grid)
         COLS = len(grid[0])
-        
-        directions = [[1, 0], [-1, 0], [0, 1], [0, -1], [1, - 1], [1, 1], [-1, 1], [-1, -1]]
-
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1),
+                    (1, -1), (1, 1), (-1, 1), (-1, -1)]
+        shortest = 1
         queue = deque()
         queue.append((0, 0))
         visit = set()
         visit.add((0, 0))
-        length = 1 
-
-        if grid[0][0] == 1:
-            return -1
+        if not grid: return -1
+        if grid[0][0] == 1: return -1 
 
         while queue:
-            
             for i in range(len(queue)):
                 r, c = queue.popleft()
-
                 if r == ROWS - 1 and c == COLS - 1:
-                    return length
-                
+                    return shortest
+
                 for dr, dc in directions:
-                    newrow = dr + r 
-                    newcol = dc + c
-                    if (newrow < 0 or newcol < 0 or
-                        newrow == ROWS or newcol == COLS or
-                        (newrow, newcol) in visit or
-                        grid[newrow][newcol] == 1):
-                        continue
+                    row, col = dr + r, dc + c
+
                     
-                    queue.append((newrow, newcol))
-                    visit.add((newrow, newcol))
-            length += 1
-        
-        return -1
+                    if (row < 0 or col < 0 or row == ROWS or col == COLS or
+                        (row, col) in visit or grid[row][col] == 1):
+                        continue
+
+                    visit.add((row, col)) 
+                    queue.append((row, col))
+            shortest += 1
+        return -1 
