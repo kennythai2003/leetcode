8a4374c789af4f906ceb703dc6723488ab7fb9b5
Kenny Thai <thaikenny2003@gmail.com>
2024-07-29 18:43:57 -0700

    Time: 252 ms (48.12%), Space: 24.3 MB (20.98%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,41 +1,35 @@
-class Solution(object):
-    def numIslands(self, grid):
-        """
-        :type grid: List[List[str]]
-        :rtype: int
-        """
-
-        if not grid: 
-            return 0
-
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
         visit = set()
         islands = 0
-                
+        directions = [[1,0], [-1,0], [0,1],[0, -1]]
+        if not grid: return 0
+        def bfs(r, c):
+            queue = deque()
+            queue.append((r, c))
+            visit.add((r, c))
+            
+            while queue:
+                # for each element in the queue, pop it and explore its neighbors
+                for i in range(len(queue)):
+                    row, col = queue.popleft()
+                    
+                    # explore neighbors
+                    for dr, dc in directions:
+                        newr, newc = row + dr, col + dc
+
+                        if (newr < 0 or newc < 0 or newr == ROWS or newc == COLS or
+                            grid[newr][newc] == "0" or (newr, newc) in visit):
+                            continue
+                        
+                        queue.append((newr, newc))
+                        visit.add((newr, newc))
+
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == "1" and (r, c) not in visit:
-                    self.bfs(grid, r, c, visit)
                     islands += 1
+                    bfs(r, c)
+        
         return islands
-    
-    def bfs(self, grid, r, c, visit):
-        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-        ROWS, COLS = len(grid), len(grid[0])
-        queue = deque()
-        queue.append((r, c))
-        visit.add((r, c))
-
-        while queue:
-            row, col = queue.popleft()
-
-            for dr, dc in directions:
-                r,c = row + dr, col + dc
-                
-                if (r < 0 or c < 0 or
-                    r == ROWS or c == COLS or
-                    (r, c) in visit or grid[r][c] == "0"):
-                    continue
-                    
-                queue.append((r, c))
-                visit.add((r, c))
