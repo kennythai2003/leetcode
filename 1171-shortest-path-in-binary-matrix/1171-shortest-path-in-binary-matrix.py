af78be4a4e2ae2f52d74e956888292bf1e0dff01
Kenny Thai <thaikenny2003@gmail.com>
2024-08-20 16:06:24 -0700

    Time: 444 ms (54.93%), Space: 18.2 MB (14.32%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
@@ -1,32 +1,38 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, -1), (-1, -1), (1, 1), (-1, 1)]
+        short = 1
+
         ROWS = len(grid)
         COLS = len(grid[0])
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1),
-                    (1, -1), (1, 1), (-1, 1), (-1, -1)]
-        shortest = 1
-        queue = deque()
-        queue.append((0, 0))
+
+        q = deque()
         visit = set()
-        visit.add((0, 0))
+
         if not grid: return -1
-        if grid[0][0] == 1: return -1 
 
-        while queue:
-            for i in range(len(queue)):
-                r, c = queue.popleft()
+        if grid[0][0] == 1:
+            return -1 
+
+        q.append((0, 0))
+        visit.add((0, 0))
+
+        while q:
+            
+            for i in range(len(q)):
+                r, c = q.popleft()
                 if r == ROWS - 1 and c == COLS - 1:
-                    return shortest
-
+                    return short
+                
                 for dr, dc in directions:
-                    row, col = dr + r, dc + c
+                    row, col = r + dr, c + dc
 
-                    
-                    if (row < 0 or col < 0 or row == ROWS or col == COLS or
-                        (row, col) in visit or grid[row][col] == 1):
+                    if (row == ROWS or col == COLS or row < 0 or col < 0
+                        or grid[row][col] == 1 or (row, col) in visit): 
                         continue
+                    
+                    q.append((row, col))
+                    visit.add((row, col))
 
-                    visit.add((row, col)) 
-                    queue.append((row, col))
-            shortest += 1
+            short += 1
         return -1 
