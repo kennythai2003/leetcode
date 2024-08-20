c6d39788484518a9a5af960de46269d7d07b2f18
Kenny Thai <thaikenny2003@gmail.com>
2024-08-20 16:38:23 -0700

    Time: 261 ms (38.12%), Space: 24.2 MB (31.1%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,35 +1,36 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        ROWS, COLS = len(grid), len(grid[0])
+        ROWS = len(grid)
+        COLS = len(grid[0])
+        
+        DIRECTIONS = [(1, 0), (-1 , 0), (0, 1), (0, -1)]
+
         visit = set()
         islands = 0
-        directions = [[1,0], [-1,0], [0,1],[0, -1]]
-        if not grid: return 0
+        if not grid: return 0 
+
         def bfs(r, c):
             queue = deque()
             queue.append((r, c))
             visit.add((r, c))
-            
             while queue:
-                # for each element in the queue, pop it and explore its neighbors
                 for i in range(len(queue)):
-                    row, col = queue.popleft()
-                    
-                    # explore neighbors
-                    for dr, dc in directions:
-                        newr, newc = row + dr, col + dc
+                    r, c = queue.popleft()
 
-                        if (newr < 0 or newc < 0 or newr == ROWS or newc == COLS or
-                            grid[newr][newc] == "0" or (newr, newc) in visit):
+                    for dr, dc in DIRECTIONS:
+                        row = dr + r
+                        col = dc + c
+
+                        if row == ROWS or col == COLS or row < 0 or col < 0 or (row, col) in visit or grid[row][col] == "0":
                             continue
                         
-                        queue.append((newr, newc))
-                        visit.add((newr, newc))
+                        queue.append((row, col))
+                        visit.add((row, col))
 
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == "1" and (r, c) not in visit:
-                    islands += 1
                     bfs(r, c)
-        
+                    islands += 1
+
         return islands
