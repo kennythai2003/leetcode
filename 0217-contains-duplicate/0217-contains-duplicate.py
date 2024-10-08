4bd081c254ef80ea492a7b77b85af963ec973b25
Kenny Thai <thaikenny2003@gmail.com>
2024-10-08 00:33:19 -0700

    Time: 435 ms (53.14%), Space: 25.8 MB (78.21%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,10 +1,15 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        prevdiffs = {}
-        
-        for num in nums:
-            if num in prevdiffs:
-                return True
-            prevdiffs[num] = 1
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        nummap = {}
 
+        for n in nums:
+
+            if n in nummap:
+                return True
+            nummap[n] = 1
+        
         return False
