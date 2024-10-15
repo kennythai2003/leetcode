0a2d6ea399e771ca3483398360f52b6c716fc778
Kenny Thai <thaikenny2003@gmail.com>
2024-10-15 01:29:04 -0700

    Time: 45 ms (75.33%), Space: 16.9 MB (58.35%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,19 +1,19 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+
         smap, tmap = {}, {}
 
         if len(s) != len(t):
             return False
-
-        for i in s:
-            smap[i] = 1 + smap.get(i, 0)
         
-        for i in t:
-            tmap[i] = 1 + tmap.get(i, 0)
+        for c in s:
+            smap[c] = 1 + smap.get(c, 0)
         
-
-        for j in smap:
-            if smap[j] != tmap.get(j , 0):
+        for c in t:
+            tmap[c] = 1 + tmap.get(c, 0)
+        
+        for i in smap:
+            if smap[i] != tmap.get(i, 0):
                 return False
-
+        
         return True
