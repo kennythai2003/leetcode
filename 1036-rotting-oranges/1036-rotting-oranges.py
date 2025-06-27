dc2bcf946759fa55aaefc5870c47dcebf4b8aa15
Kenny Thai <thaikenny2003@gmail.com>
2025-06-26 18:02:30 -0700

    Time: 3 ms (82.01%), Space: 18 MB (26.15%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,35 +1,41 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        ROWS = len(grid)
-        COLS = len(grid[0])
-        
-        fresh = 0
-        mins = 0
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
-        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        queue = deque() 
+        # visit = set()
+
+        fresh = 0
+        time = 0
 
         if not grid:
             return -1
-
-        queue = deque()
-        for i in range(ROWS):
-            for j in range(COLS):
-                if grid[i][j] == 1:
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == 1:
                     fresh += 1
-                elif grid[i][j] == 2:
-                    queue.append((i, j))
-
+                if grid[r][c] == 2:
+                    queue.append((r, c))
+        
         while queue and fresh > 0:
+
             for i in range(len(queue)):
+
                 row, col = queue.popleft()
                 for dr, dc in directions:
-                    newrow, newcol = row + dr, col + dc
-                    if (newrow < 0 or newcol < 0 or newrow >= ROWS or newcol >= COLS or grid[newrow][newcol] == 2 or grid[newrow][newcol] == 0):
+                    drow = row + dr
+                    dcol = col + dc
+                    if (min(drow, dcol) < 0 or drow == len(grid) or dcol == len(grid[0]) or
+                    grid[drow][dcol] == 0 or  grid[drow][dcol] == 2):
                         continue
-                    
-                    grid[newrow][newcol] = 2
-                    queue.append((newrow, newcol))
-                    fresh -= 1
-            mins +=  1
 
-        return mins if not fresh else -1
+                    grid[drow][dcol] = 2
+                    queue.append((drow, dcol))
+                    fresh -= 1
+                    # visit.add((drow,dcol))
+                
+            time += 1
+            
+        return time if not fresh else -1
+
+                
