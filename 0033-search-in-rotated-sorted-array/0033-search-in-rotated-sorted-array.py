c7e97a4bf8bb094f31d0c26741799fcf33210487
Kenny Thai <thaikenny2003@gmail.com>
2025-07-02 18:44:08 -0700

    Time: 0 ms (100%), Space: 18.3 MB (22.14%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -7,16 +7,18 @@ class Solution:
 
             if nums[m] == target:
                 return m
-
+            
             if nums[l] <= nums[m]:
                 if nums[l] <= target <= nums[m]:
-                    r= m - 1
+                    r = m - 1
                 else:
                     l = m + 1
             else:
                 if nums[m] <= target <= nums[r]:
-                    l =m + 1
+                    l = m + 1
                 else:
-                    r= m - 1
+                    r = m - 1
+         
         
         return -1
+            
