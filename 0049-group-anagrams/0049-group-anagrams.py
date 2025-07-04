e29ddd75d419186082e99f206f2277df4b10a0f1
Kenny Thai <thaikenny2003@gmail.com>
2025-07-04 11:33:20 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,13 +1,11 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        
         res = defaultdict(list)
-
         for s in strs:
-            alpas = [0] * 26
+            cmap = [0] * 26
             for c in s:
-                alpas[ord(c) - ord('a')] += 1
-            
-            res[tuple(alpas)].append(s)
-        
+                cmap[ord(c) - ord('a')] += 1
+
+            res[tuple(map)].append(s)
+
         return res.values()
