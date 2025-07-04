34861b6f18c05e8cf871e8a165bc1413da87cebc
Kenny Thai <thaikenny2003@gmail.com>
2025-07-04 10:47:26 -0700

    Time: 7 ms (89.57%), Space: 17.7 MB (97.51%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,13 +1,12 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-
         smap, tmap = {}, {}
 
         if len(s) != len(t):
             return False
-        
+
         for c in s:
-            smap[c] = 1 + smap.get(c, 0)
+            smap[c] = 1 + smap.get(c, 0) 
         
         for c in t:
             tmap[c] = 1 + tmap.get(c, 0)
@@ -15,5 +14,5 @@ class Solution:
         for i in smap:
             if smap[i] != tmap.get(i, 0):
                 return False
-        
+
         return True
