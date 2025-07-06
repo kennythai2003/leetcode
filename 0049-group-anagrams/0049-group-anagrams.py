c635f9928db9acde38d0563fad178a7ecb1fa6c8
Kenny Thai <thaikenny2003@gmail.com>
2025-07-06 15:01:39 -0700

    Time: 15 ms (50.11%), Space: 22.7 MB (26.04%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -3,12 +3,9 @@ class Solution:
         smap = defaultdict(list)
 
         for s in strs:
-
-            chars = [0] * 26
-
+            charcnt = [0] * 26
             for c in s:
-                chars[ord(c) - ord('a')] += 1
-            
-            smap[tuple(chars)].append(s)
+                charcnt[ord(c) - ord('a')] += 1
+            smap[tuple(charcnt)].append(s)
         
-        return smap.values()
+        return list(smap.values())
