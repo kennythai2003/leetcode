34930ac587a7159e2d5dbf77f65e8d075a50dabe
Kenny Thai <thaikenny2003@gmail.com>
2025-07-07 17:14:28 -0700

    Time: 59 ms (69.22%), Space: 22.8 MB (56.66%) - LeetHub

--- 0937-online-stock-span/0937-online-stock-span.py
+++ 0937-online-stock-span/0937-online-stock-span.py
@@ -1,15 +1,15 @@
 class StockSpanner:
 
     def __init__(self):
-        self.stack = []  # Each element is a pair: [price, span]
+        self.stack = []
 
     def next(self, price: int) -> int:
         span = 1
-        # While the current price is greater than or equal to the price at the top of the stack
         while self.stack and self.stack[-1][0] <= price:
-            span += self.stack.pop()[1]  # Add their span to ours
+            span += self.stack.pop()[1]
         self.stack.append([price, span])
         return span
+
 
 # Your StockSpanner object will be instantiated and called as such:
 # obj = StockSpanner()
