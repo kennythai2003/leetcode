6b57fe4d0545adbce345b279ef61c170b853c213
Kenny Thai <thaikenny2003@gmail.com>
2025-07-11 12:28:06 -0700

    Time: 667 ms (30.24%), Space: 20.8 MB (36.68%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -3,25 +3,24 @@ class Solution:
         nums.sort()
         res = []
 
-        for i, v in enumerate(nums):
+        for i, n in enumerate(nums):
 
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
             
-            l = i + 1
-            r = len(nums) - 1
+
+            l, r = i + 1, len(nums) - 1
 
             while l < r:
 
-                if nums[l] + nums[r] + v < 0:
+                if nums[l] + nums[r] + n < 0:
                     l += 1
-                elif nums[l] + nums[r] + v > 0:
+                elif nums[l] + nums[r] + n > 0:
                     r -= 1
                 else:
-                    res.append([nums[l], nums[r], v])
+                    res.append([nums[l], nums[r], n])
                     l += 1
                     while l < r and nums[l] == nums[l - 1]:
                         l += 1
             
-        
         return res
