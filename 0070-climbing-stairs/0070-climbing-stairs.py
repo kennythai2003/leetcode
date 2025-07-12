3fbe73a32ff7a3472503950f186f91762291311c
Kenny Thai <thaikenny2003@gmail.com>
2025-07-12 14:28:41 -0700

    Time: 3 ms (2.86%), Space: 17.6 MB (78.41%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,11 +1,11 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        one, two = 1, 1
+        dp = [0] * (n + 1)
+        dp[0] = 1  # One way to stand at the ground (0th step)
+        dp[1] = 1  # One way to reach the 1st step
 
-        for i in range(n - 1):
-            temp = one
-            one = one + two
-            two = temp
-        
+        for stair in range(2, n + 1):
+            for step in range(1, 3):  # step = 1 or 2
+                dp[stair] += dp[stair - step]
 
-        return one
+        return dp[n]
