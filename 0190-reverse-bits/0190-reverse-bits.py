2e8b3fd90a006b8f8cb1f70dcbd403d4602ac605
Kenny Thai <thaikenny2003@gmail.com>
2025-07-16 16:32:44 -0700

    Time: 36 ms (75.7%), Space: 17.8 MB (55.9%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,9 +1,9 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
         res = 0
-
+        bit = 0
         for i in range(32):
-            bit = (n >> i) & 1
+            bit = 1 & (n >> i)
             res = res | (bit << 31 - i)
         
         return res
