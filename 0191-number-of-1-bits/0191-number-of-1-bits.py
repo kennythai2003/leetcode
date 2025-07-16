6976c562dcb101c21055237fff65671fc6e9e4bf
Kenny Thai <thaikenny2003@gmail.com>
2025-07-16 16:11:38 -0700

    Time: 0 ms (100%), Space: 17.6 MB (81.29%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -4,7 +4,7 @@ class Solution:
         res = 0
 
         while n: 
-            res += n % 2
-            n = n >> 1
+            n = n & (n - 1)
+            res += 1
         
         return res
