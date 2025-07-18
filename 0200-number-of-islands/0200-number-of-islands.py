ffff4b34965305d0524cb3d3944ca764d7d7fbc6
Kenny Thai <thaikenny2003@gmail.com>
2025-07-18 14:04:30 -0700

    Time: 297 ms (14.6%), Space: 25.4 MB (26.81%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,10 +1,16 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)] 
-        visit = set()
-        islands = 0
+        
 
-        def bfs (r, c):
+        islands = 0
+        visit = set()
+        directions = [(1, 0), (-1, 0), (0, -1), (0, 1)]
+
+        if not grid: return islands
+
+
+
+        def bfs(r, c):
             queue = deque()
             queue.append((r, c))
             visit.add((r, c))
@@ -12,12 +18,14 @@ class Solution:
                 for i in range(len(queue)):
                     row, col = queue.popleft()
                     for dr, dc in directions:
-                        drow, dcol = row + dr, col + dc
-                        if (min(drow, dcol) < 0 or drow == len(grid) or dcol == len(grid[0]) or (drow, dcol) in visit or grid[drow][dcol] == "0"):
+                        drow, dcol = dr + row, dc + col
+                        if (min(drow, dcol) < 0 or drow == len(grid) or dcol == len(grid[0])
+                            or grid[drow][dcol] == "0" or (drow, dcol) in visit):
                             continue
                         
                         queue.append((drow, dcol))
                         visit.add((drow, dcol))
+
 
         for r in range(len(grid)):
             for c in range(len(grid[0])):
