93401a1fcc7f8a4f11624a878759f5c95a7b8a0c
Kenny Thai <thaikenny2003@gmail.com>
2025-08-15 11:42:39 -0700

    Time: 0 ms (100%), Space: 17.6 MB (98.4%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -7,7 +7,6 @@
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         res = []
-        
         if not root:
             return res
         
@@ -15,16 +14,18 @@ class Solution:
         queue.append(root)
 
         while queue:
+
             right = None
             for i in range(len(queue)):
+
                 curr = queue.popleft()
 
                 if curr:
                     queue.append(curr.left)
                     queue.append(curr.right)
                     right = curr
-            
             if right:
                 res.append(right.val)
         
         return res
+            
