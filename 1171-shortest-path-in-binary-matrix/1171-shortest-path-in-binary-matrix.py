fbdb3649c6a63da0b749f96ea83d7c517a4727cb
Kenny Thai <thaikenny2003@gmail.com>
2025-08-15 12:35:10 -0700

    Time: 303 ms (15.26%), Space: 19.6 MB (17.59%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
@@ -1,32 +1,35 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        directions = [(-1, 0), (1, 0), (0, -1), (0, 1), (1, 1), (1, - 1), (-1, 1), (-1, -1)]
-
-        if grid[0][0] == 1 or not grid:
-            return -1 
         
+
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, -1), (1, 1), (-1, 1), (-1, -1)]
+
+        if not grid or grid[0][0] == 1:
+            return -1
+        
+        length = 1
         queue = deque()
-        visit = set()
         queue.append((0, 0))
+        
+        visit = set()
         visit.add((0, 0))
-        path = 1
 
         while queue:
+
             for i in range(len(queue)):
-
                 r, c = queue.popleft()
-                if (r == len(grid) - 1) and (c == len(grid[0]) - 1):
-                    return path
+                if r == len(grid) - 1 and c == len(grid[0]) - 1:
+                    return length
                 for dr, dc in directions:
-                    row = dr + r
-                    col = dc + c
-
-                    if (min(row, col) < 0 or row == len(grid) or col == len(grid[0]) or grid[row][col] == 1 or (row, col) in visit):
+                    row, col = r + dr, c + dc
+                    if (min(row, col) < 0 or row == len(grid) or col == len(grid[0]) or
+                        grid[row][col] == 1 or ((row, col) in visit)):
                         continue
-                    
+                
                     queue.append((row, col))
                     visit.add((row, col))
-                
-            path += 1
+                    
+            length += 1
         
         return -1
+
