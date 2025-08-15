70619c376dbc6dba14605041a270d1e4fd448aa7
Kenny Thai <thaikenny2003@gmail.com>
2025-08-15 11:48:20 -0700

    Time: 301 ms (13.8%), Space: 25.5 MB (22.5%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,29 +1,31 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         
+        res = 0
         visit = set()
-        islands = 0
         directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-
-        def bfs(rd, cd):
+        def bfs(i, j):
             queue = deque()
-            queue.append((rd, cd))
+            queue.append((i, j))
+            visit.add((i, j))
+
             while queue:
                 for i in range(len(queue)):
                     r, c = queue.popleft()
-
                     for dr, dc in directions:
-                        row, col = r + dr, c + dc
-                        if (min(row, col) < 0 or row == len(grid) or col == len(grid[0]) or
-                            (row, col) in visit or grid[row][col] == "0"):
+                        row, col = dr + r, dc + c
+                        if (min(row, col) < 0 or row == len(grid) or col == len(grid[0]) 
+                            or grid[row][col] == "0" or (row, col) in visit):
                             continue
+                        
                         queue.append((row, col))
                         visit.add((row, col))
-
-        for r in range(len(grid)):
-            for c in range(len(grid[0])):
-                if (r, c) not in visit and grid[r][c] == "1":
-                    bfs(r, c)
-                    islands += 1
         
-        return islands
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+
+                if grid[i][j] == "1" and (i, j) not in visit:
+                    bfs(i, j)
+                    res += 1
+        
+        return res
