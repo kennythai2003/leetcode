f1faca6cbb7452a56ca03cb6b683745f54c2d3f0
Kenny Thai <thaikenny2003@gmail.com>
2025-08-15 11:51:51 -0700

    Time: 33 ms (24.28%), Space: 18.1 MB (87.63%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,36 +1,32 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        islands = 0
+        res = 0
         visit = set()
-        directions = [(1, 0), (-1, 0), (0, -1), (0, 1)]
-
-        if not grid: return islands
-
-
-
-        def bfs(r, c):
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        def bfs(i, j):
             queue = deque()
-            queue.append((r, c))
-            visit.add((r, c))
+            queue.append((i, j))
+            visit.add((i, j))
             area = 1
             while queue:
                 for i in range(len(queue)):
-                    row, col = queue.popleft()
+                    r, c = queue.popleft()
                     for dr, dc in directions:
-                        drow, dcol = dr + row, dc + col
-                        if (min(drow, dcol) < 0 or drow == len(grid) or dcol == len(grid[0])
-                            or grid[drow][dcol] == 0 or (drow, dcol) in visit):
+                        row, col = dr + r, dc + c
+                        if (min(row, col) < 0 or row == len(grid) or col == len(grid[0]) 
+                            or grid[row][col] == 0 or (row, col) in visit):
                             continue
                         
-                        queue.append((drow, dcol))
-                        visit.add((drow, dcol))
+                        queue.append((row, col))
+                        visit.add((row, col))
                         area += 1
+
             return area
 
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
 
-        for r in range(len(grid)):
-            for c in range(len(grid[0])):
-                if grid[r][c] == 1 and (r, c) not in visit:
-                    islands = max(islands, bfs(r, c))
+                if grid[i][j] == 1 and (i, j) not in visit:
+                    res = max(bfs(i, j), res)
         
-        return islands
+        return res
