cd679ea5f5237aa4f2b86c7e8e03e1e39585fb4a
Kenny Thai <thaikenny2003@gmail.com>
2025-09-03 13:20:33 -0700

    Time: 31 ms (24.26%), Space: 17.8 MB (55.99%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -1,7 +1,5 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        
-
         res = []
         candidates.sort()
         def dfs(i, curr, total):
@@ -10,7 +8,7 @@ class Solution:
                 res.append(curr.copy())
                 return
             
-            if total > target or i >= len(candidates):
+            if i >= len(candidates) or total > target:
                 return
             
             curr.append(candidates[i])
