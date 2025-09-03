0f941408cefebd297b1684ca55abe7c1f9c785b9
Kenny Thai <thaikenny2003@gmail.com>
2025-09-03 13:19:14 -0700

    Time: 0 ms (100%), Space: 17.7 MB (91.14%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,11 +1,8 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        
         res = []
-
         subsets = []
         def dfs(i):
-
             if i >= len(nums):
                 res.append(subsets.copy())
                 return
