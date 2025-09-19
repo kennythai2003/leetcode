2a5874e131313c2cd203982335ded6c093145332
Kenny Thai <thaikenny2003@gmail.com>
2025-09-18 19:58:46 -0700

    Time: 19 ms (52.77%), Space: 17.7 MB (81.85%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -2,7 +2,6 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         char_set = set()
         res = 0
-
         l = 0
 
         for r in range(len(s)):
@@ -11,8 +10,7 @@ class Solution:
                 char_set.remove(s[l])
                 l += 1
             
-            char_set.add(s[r])
-
             res = max(res, r - l + 1)
+            char_set.add(s[r])
         
         return res
