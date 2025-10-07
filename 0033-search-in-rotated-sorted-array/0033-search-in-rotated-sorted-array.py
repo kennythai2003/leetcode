64c277a1c1c5f7a0d3b6bff49b4788ede80907c6
Kenny Thai <thaikenny2003@gmail.com>
2025-10-06 17:34:31 -0700

    Time: 0 ms (100%), Space: 18 MB (91.16%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -5,11 +5,13 @@ class Solution:
 
         while l <= r:
 
-            m = (l + r) // 2
+            m = l + ((r - l) // 2)
 
             if nums[m] == target:
                 return m
+            
             if nums[l] <= nums[m]:
+
                 if nums[l] <= target <= nums[m]:
                     r = m - 1
                 else:
@@ -19,5 +21,5 @@ class Solution:
                     l = m + 1
                 else:
                     r = m - 1
-        return -1
-            
+        
+        return -1 
