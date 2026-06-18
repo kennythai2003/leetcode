b35c73c37ddfd9c2f5ec24f1069fcdcc30d5adfd
Kenny Thai <thaikenny2003@gmail.com>
2026-06-17 18:11:05 -0700

    Time: 7 ms (51.99%), Space: 24.8 MB (12.42%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,16 +1,15 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        nummap = {}
+        nums_map = {}
         res = []
         for n in nums:
-            nummap[n] = 1 + nummap.get(n, 0)
+            nums_map[n] = 1 + nums_map.get(n, 0)
         
         freq = [[] for i in range(len(nums) + 1)]
-        for i, v in nummap.items():
+        for i, v in nums_map.items():
             freq[v].append(i)
         
         for i in range(len(freq) - 1, -1, -1):
-
             for j in freq[i]:
                 res.append(j)
                 if len(res) == k:
