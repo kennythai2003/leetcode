f94b62b1eff7a9dd84344ba8ab4f93e67a1e49b8
Kenny Thai <thaikenny2003@gmail.com>
2026-06-18 17:43:09 -0700

    Time: 3 ms (30.89%), Space: 19.5 MB (9.8%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -5,10 +5,12 @@ class Solution:
         def backtrack (openN, closeN):
             if openN == closeN == n:
                 res.append("".join(stack))
+                return
             if openN < n:
                 stack.append("(")
                 backtrack(openN + 1, closeN)
-            if closeN < n:
+                stack.pop()
+            if closeN < openN:
                 stack.append(")")
                 backtrack(openN, closeN + 1)
                 stack.pop()
